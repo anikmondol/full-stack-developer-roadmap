ba1e7ddf0d33349f1dbb1454add62f65c5000581
anikmondol <anikmondol558363@gmail.com>
2024-12-08 20:57:09 +0600

    full-stack-developer-roadmap advance javascript tutorial-12 video completed.

---
+++ advance-javascript/11-modules/library.js
@@ -0,0 +1,30 @@
+// export default function(name){
+//     console.log(`this name is ${name}`);
+    
+// }
+
+// let message = "hello world";
+
+
+// function user(name){
+//     console.log(`hello user ${name}`);
+    
+// };
+
+// class Test{
+//     constructor(){
+//         console.log("this constructor method");
+        
+//     }
+// };
+
+
+// export { message, user, Test };
+
+
+function user(name){
+    console.log(`hello user ${name}`);
+    
+};
+
+export { user};
---
+++ advance-javascript/11-modules/main.js
@@ -0,0 +1,22 @@
+// import {default as fun} from "./library.js";
+// import fun from "./library.js";
+
+// fun("anik");
+
+// import { message as m, user, Test } from "./library.js";
+
+// import * as an from "./library.js"
+
+// document.body.innerHTML = an.message;
+
+
+// console.log(an.message);
+
+// an.user("anik");
+
+// let a = new an.Test();
+
+
+import {user} from "./bridge.js";
+
+user("anik");
