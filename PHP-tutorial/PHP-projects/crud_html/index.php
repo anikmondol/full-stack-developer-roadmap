03eafb35375f808ba3af89094728068e10e49dff
anikmondol <anikmondol558363@gmail.com>
2024-12-21 20:56:31 +0600

    full-stack-developer-roadmap PHP MySQL CRUD projects read part done.

--- PHP-tutorial/PHP-projects/crud_html/index.php
+++ PHP-tutorial/PHP-projects/crud_html/index.php
@@ -3,74 +3,50 @@ include 'header.php';
 ?>
 <div id="main-content">
     <h2>All Records</h2>
-    <table cellpadding="7px">
-        <thead>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Class</th>
-        <th>Phone</th>
-        <th>Action</th>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Ramesh</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Suresh</td>
-                <td>Punjab</td>
-                <td>BCOM</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Suresh</td>
-                <td>Haryana</td>
-                <td>BSC</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Krishna</td>
-                <td>Gujrat</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Rohit</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <?php
+
+    $conn = mysqli_connect("localhost", "root", "", "php_crud") or die("connection failed");
+
+    $sql = "SELECT * FROM students JOIN studentclass where students.sclass = studentclass.cid";
+    $result = mysqli_query($conn, $sql) or die("query unsuccessful.");
+
+    if (mysqli_num_rows($result) > 0) {
+
+    ?>
+        <table cellpadding="7px">
+            <thead>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Class</th>
+                <th>Phone</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                <?php while ($row = mysqli_fetch_assoc($result)) {
+
+                ?>
+                    <tr>
+                        <td><?= $row["sid"] ?></td>
+                        <td><?= $row["sname"] ?></td>
+                        <td><?= $row["saddress"] ?></td>
+                        <td><?= $row["cname"] ?></td>
+                        <td><?= $row["sphone"] ?></td>
+                        <td>
+                            <a href='edit.php'>Edit</a>
+                            <a href='delete-inline.php'>Delete</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    <?php } else { ?>
+        <?php echo "<th> No date found </th>"; ?>
+    <?php }
+    mysqli_close($conn);
+    ?>
 </div>
 </div>
 </body>
+
 </html>
