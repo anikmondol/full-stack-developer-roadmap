bd2bb250eed89ab08a9074ea25686dd5e802ee3a
anikmondol <anikmondol558363@gmail.com>
2025-01-02 09:50:14 +0600

    full-stack-developer-roadmap PHP Ajax - Drag & Drop File Upload with DropZonejs

---
+++ PHP-Rest-Api/drag_drop_upload/upload.php
@@ -0,0 +1,47 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "phptutorial") or die("connection failed" . mysqli_connect_error());
+
+
+
+
+
+if ($_FILES['file']['name'] != "") {
+
+	$file_names = '';
+
+	$total = count($_FILES['file']['name']);
+
+	for ($i = 0; $i < $total; $i++) {
+		$filename = $_FILES['file']['name'][$i];
+
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+
+		$valid_extensions = array("png", "jpg", "jpeg");
+
+
+		if (in_array($extension, $valid_extensions)) {
+
+			$new_name = rand() . "." . $extension;
+
+			$path = "images/" . $new_name;
+
+			move_uploaded_file($_FILES['file']['tmp_name'][$i], $path);
+
+			$file_names .= $new_name . " , ";
+		} else {
+			echo "false";
+		}
+	}
+	$sql = "INSERT INTO `uploads`(`file_name`) VALUES ('$file_names')";
+
+	if (mysqli_query($conn, $sql)) {
+		echo "true";
+	} else {
+		echo "false";
+	}
+	
+
+
+}
