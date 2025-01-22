e9b9f0be2f4689369ca3c47c43731e579b15926d
anikmondol <anikmondol558363@gmail.com>
2025-01-22 19:58:24 +0600

    laravel-tutraula video-16 done

--- Laravel/laravel-tutorial/routes/web.php
+++ Laravel/laravel-tutorial/routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-
-
-Route::get('/post', function () {
-    return view('post');
-});
-
-
-Route::get('/test1', function () {
-    return view('test');
-});
-
-
-
+Route::get('/', [PageController::class, 'showHome'])->name("home");
+Route::get('/user', [PageController::class, 'showUser'])->name('users');
--- PHP-tutorial/Basic-php/1-first.php
+++ PHP-tutorial/Basic-php/1-first.php
@@ -1,17 +1,22 @@
 <!-- <?php
 
-echo "hello everyOne";
+        echo "hello everyOne";
 
-?> -->
+        ?> -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php echo "hello"; ?>
+    <?php
+    phpinfo();
+    ?>
 </body>
+
 </html>
