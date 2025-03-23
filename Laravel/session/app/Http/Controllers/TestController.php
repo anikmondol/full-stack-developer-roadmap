c37c9c4d243aac48dcbfcf94c86aee103d1d0b43
anikmondol <anikmondol558363@gmail.com>
2025-03-23 22:25:45 +0600

    video number 57 done.

--- Laravel/session/app/Http/Controllers/TestController.php
+++ Laravel/session/app/Http/Controllers/TestController.php
@@ -11,37 +11,84 @@ class TestController extends Controller
     public function index()
     {
 
-        $value = session()->all();
-        echo "<pre>";
-        print_r($value);
-        echo "</pre>";
+        // $value = session()->all();
+        // echo "<pre>";
+        // print_r($value);
+        // echo "</pre>";
+
+        // $value = session()->except(['name', '_previous']);
+        // echo "<pre>";
+        // print_r($value);
+        // echo "</pre>";
+
+
+        // $value = session()->only(['name', 'class']);
+        // echo "<pre>";
+        // print_r($value);
+        // echo "</pre>";
 
         // $value = session()->get('name');
         // $value = session('name');
-
-
         // echo $value;
+
+
+        // $value = session('name', "hello");
+        // echo $value;
+
+
+        // if (session()->has('name')) {
+        //     $value = session('name');
+        //     echo $value;
+        // } else {
+        //     echo "Name key doesn't Exists";
+        // }
+
+        // if (session()->exists('name')) {
+        //     $value = session('name');
+        //     echo $value;
+        // } else {
+        //     echo "Name key doesn't Exists";
+        // }
+
+
+        $value = session('name');
+
+        return view('welcome', compact('value'));
+
+
     }
 
 
-    public function storeSession(Request $request) {
+    public function storeSession(Request $request)
+    {
 
         session([
-            'name' => 'YahooBaba',
+            'name' => "YahooBaba",
             "class" => "Btech"
         ]);
 
 
-        return redirect('/');
+        // session()->increment('count', $incrementBy = 2);
 
+        // session()->decrement('count', $incrementBy = 2);
+
+        // session()->regenerate();
+
+
+        session()->flash('status', 'session saved successfully');
+
+        return redirect('/');
     }
 
 
-    public function deleteSession() {
+    public function deleteSession()
+    {
 
         // session()->forget(['name', 'class']);
 
-        session()->flush();
+        // session()->flush();
+
+        session()->invalidate();
 
         return redirect('/');
     }
